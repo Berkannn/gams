a791f75767a0bebd039496179f72e5d971b587eb
James Edmondson <jedmondson@gmail.com>
2014-07-28 04:09:25 -0400

    * JAVA: Added Position and GPSPosition to com.gams.utility   * Added jni port for corresponding classes * Updated to version 0.2.10

---
+++ port/java/src/com/gams/utility/Position.java
@@ -0,0 +1,163 @@
+/**
+ * Copyright(c) 2013-2014 Carnegie Mellon University. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following acknowledgments and disclaimers.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ * 
+ * 3. The names “Carnegie Mellon University,” "SEI” and/or “Software
+ *  Engineering Institute" shall not be used to endorse or promote products
+ *  derived from this software without prior written permission. For written
+ *  permission, please contact permission@sei.cmu.edu.
+ * 
+ * 4. Products derived from this software may not be called "SEI" nor may "SEI"
+ *  appear in their names without prior written permission of
+ *  permission@sei.cmu.edu.
+ * 
+ * 5. Redistributions of any form whatsoever must retain the following
+ *  acknowledgment:
+ * 
+ *    This material is based upon work funded and supported by the Department
+ *    of Defense under Contract No. FA8721-05-C-0003 with Carnegie Mellon
+ *    University for the operation of the Software Engineering Institute, a
+ *    federally funded research and development center. Any opinions,
+ *    findings and conclusions or recommendations expressed in this material
+ *    are those of the author(s) and do not necessarily reflect the views of
+ *    the United States Department of Defense.
+ * 
+ *    NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING
+ *    INSTITUTE MATERIAL IS FURNISHED ON AN “AS-IS” BASIS. CARNEGIE MELLON
+ *    UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR
+ *    IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF
+ *    FITNESS FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS
+ *    OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES
+ *    NOT MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT,
+ *    TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+ * 
+ *    This material has been approved for public release and unlimited
+ *    distribution.
+ **/
+package com.gams.utility;
+
+import com.gams.GamsJNI;
+
+public class Position extends GamsJNI
+{
+  private native long jni_Position();
+  private native long jni_Position(long cptr);
+  private native long jni_Position(long cptr, double lat, double lon, double alt);
+  private static native void jni_freePosition(long cptr);
+  private native java.lang.String jni_toString(long cptr);
+  private native double jni_getX(long cptr);
+  private native double jni_getY(long cptr);
+  private native double jni_getZ(long cptr);
+  private native void jni_setX(long cptr, double input);
+  private native void jni_setY(long cptr, double input);
+  private native void jni_setZ(long cptr, double input);
+
+  private boolean manageMemory = true;
+
+  public Position()
+  {
+    setCPtr(jni_Position());
+  }
+
+  public Position(double lat, double lon, double alt)                                   
+  {
+    setCPtr(jni_Position(lat,lon,alt));
+  }
+
+  public Position(Position input)
+  {
+    setCPtr(jni_Position(input.getCPtr()));
+  }
+
+  /**
+   * Converts the position into a string
+   * @return position as a string
+   **/
+  public java.lang.String toString()
+  {
+    String result;
+    result += getX();
+    result += ",";
+    result += getY();
+    result += ",";
+    result += getZ();
+    
+    return result;
+  }
+  
+  /**
+   * Returns the latitude
+   * @return latitude
+   **/
+  public double getX()
+  {
+    return jni_getX(getCPtr());
+  }
+  
+  /**
+   * Returns the longitude
+   * @return longitude
+   **/
+  public double getY()
+  {
+    return jni_getY(getCPtr());
+  }
+
+  /**
+   * Returns the altitude
+   * @return altitude
+   **/
+  public double getZ()
+  {
+    return jni_getZ(getCPtr());
+  }
+  
+  /**
+   * Sets the latitude
+   * @return latitude
+   **/
+  public void setX(double input)
+  {
+    jni_setX(getCPtr(),input);
+  }
+  
+  /**
+   * Sets the longitude
+   * @return longitude
+   **/
+  public void setY(double input)
+  {
+    jni_setY(getCPtr(),input);
+  }
+
+  /**
+   * Sets the altitude
+   * @return altitude
+   **/
+  public void setZ(double input)
+  {
+    jni_setZ(getCPtr(),input);
+  }
+  
+  /**
+   * Deletes the C instantiation. To prevent memory leaks, this <b>must</b> be
+   * called before an instance of WaitSettings gets garbage collected
+   */
+  public void free()
+  {
+    if(manageMemory)
+    {
+      jni_freePosition(getCPtr());
+    }
+  }                
+}
+
