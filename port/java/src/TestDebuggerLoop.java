03c360024f7af61e3bf128cd181d22607da02317
James Edmondson <jedmondson@gmail.com>
2014-08-11 10:40:41 -0400

    * JAVA: Added TestDebuggerLoop.java

---
+++ port/java/src/TestDebuggerLoop.java
@@ -0,0 +1,31 @@
+/*********************************************************************
+ * Usage of this software requires acceptance of the SMASH-CMU License,
+ * which can be found at the following URL:
+ *
+ * https://code.google.com/p/smash-cmu/wiki/License
+ *********************************************************************/
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.madara.KnowledgeBase;
+import com.gams.controllers.BaseController;
+
+
+public class TestDebuggerLoop
+{ 
+  public static void main (String...args) throws Exception
+  {
+    System.out.println("Creating knowledge base...");
+    KnowledgeBase knowledge = new KnowledgeBase();
+    System.out.println("Passing knowledge base to base controller...");
+    BaseController controller = new BaseController(knowledge);
+    
+    System.out.println("Running controller every 1s for 10s...");
+    controller.run(1.0, 10.0);
+  }
+  
+  
+}
