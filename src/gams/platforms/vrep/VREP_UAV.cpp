0335ac81c4f0baf48c5b5ef4030469470449a3ae
David Kyle <dskyle@sei.cmu.edu>
2015-06-19 10:46:23 -0400

    Missed a merge point

--- src/gams/platforms/vrep/VREP_UAV.cpp
+++ src/gams/platforms/vrep/VREP_UAV.cpp
@@ -63,7 +63,6 @@ using std::string;
 
 gams::platforms::VREP_UAV *
 gams::platforms::VREP_UAV_Factory::make_new (
-<<<<<<< HEAD
   const Madara::Knowledge_Vector & /*args*/,
   Madara::Knowledge_Engine::Knowledge_Base * knowledge,
   variables::Sensors * sensors,
