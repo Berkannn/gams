18cda4d93b3dcc617f2fa1e81ae917780b8061ea
Anton Dukeman <anton.dukeman@gmail.com>
2015-07-29 20:26:47 -0400

    typo

--- scripts/simulation/formation_coverage/formation_coverage_simulation.pl
+++ scripts/simulation/formation_coverage/formation_coverage_simulation.pl
@@ -9,9 +9,9 @@ $num = 4;
 $sim = "formation_coverage";
 $area = "small";
 $madara_debug = 0;
-$gams_debug = 5;
+$gams_debug = 6;
 $border = "region.0";
 $num_coverages = 0;
 $launch_controllers = 1;
 
-simulation::run($num, $time, $period, $sim, $area, $madara_debug, $gams_debug, $border, $num_coverages, $launch_controllers;
+simulation::run($num, $time, $period, $sim, $area, $madara_debug, $gams_debug, $border, $num_coverages, $launch_controllers);
